b4002076b52d958d65ee341c782c0c7a1f669e3d
Muhammad Ilyas <m.ilyas@live.com>
2026-01-27 19:27:56 +0500

    Refactor JwtConstants: add documentation for `KEY_SCOPE_CLAIM` and update parent version to 1.0.3 in POM.

--- src/main/java/io/github/ilyaslabs/microservice/security/guard/JwtConstants.java
+++ src/main/java/io/github/ilyaslabs/microservice/security/guard/JwtConstants.java
@@ -1,10 +1,15 @@
 package io.github.ilyaslabs.microservice.security.guard;
 
 /**
- *
- * @author Muhammad Ilyas (m.ilyas@live.com)
+ * Contains constants used for handling JSON Web Tokens (JWT) in the application.
+ * These constants are utilized primarily for claims processing and validation within JWTs.
  */
 public class JwtConstants {
 
+    /**
+     * Represents the claim key used for specifying the scope in a JSON Web Token (JWT).
+     * This constant is used to parse or validate the scope information
+     * embedded within the token.
+     */
     public static final String KEY_SCOPE_CLAIM = "scope";
 }
